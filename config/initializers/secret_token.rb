bf2a5d89a7eb1ac20929ef7ff871f87b73b9fbb8
ragaskar <ragaskar@gmail.com>
2013-01-16 07:18:46 -0800

    Add Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraJs::Application.config.secret_token = 'ce9ce492fb2febdfcf155b83e759d10c258340aae6c898832968294e33373aef57606b4e93feb4d1b5c6c30d055100a16f19147995306490c9959bd438dc17d8'
