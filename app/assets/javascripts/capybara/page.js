b5633f6bc91e937a3d53de92ee5e26a9f6c4625a
ragaskar <ragaskar@gmail.com>
2013-01-19 19:07:20 -0800

    Strings passed into Page become jQuery objects.

--- app/assets/javascripts/capybara/page.js
+++ app/assets/javascripts/capybara/page.js
@@ -3,13 +3,13 @@
     var $html = $(html);
     return $html.extend({
       fillIn: function(labelName) {
-        var label = $('label:contains(' +labelName + ')', html),
-        input = $('#' + label.attr('for'), html).add($('input', label)).first();
+        var label = $('label:contains(' +labelName + ')', $html),
+        input = $('#' + label.attr('for'), $html).add($('input', label)).first();
         if (!label.length) {
-          throw "Label with text '" + labelName + "' not found in " + html.html();
+          throw "Label with text '" + labelName + "' not found in " + $html.html();
         }
         if (!input.length) {
-          throw "Input for label '" + labelName + "' not found in " + html.html();
+          throw "Input for label '" + labelName + "' not found in " + $html.html();
         }
         return {
           with: function(val) {
--- spec/javascripts/capybara/page_spec.js
+++ spec/javascripts/capybara/page_spec.js
@@ -6,26 +6,26 @@ describe("Page", function() {
   });
   describe("fillIn", function() {
     it("fills in a given text field by label text when label[for] is used", function() {
-      var html = $('<div><label for="my_field_1">My First Field</label><input id="my_field_1" /></div>');
+      var html = '<div><label for="my_field_1">My First Field</label><input id="my_field_1" /></div>';
       page = new Capybara.Page(html);
-      expect($('#my_field_1', html).val()).toBe('');
+      expect($('#my_field_1', page).val()).toBe('');
       page.fillIn('My First Field').with('Foobar');
-      expect($('#my_field_1', html).val()).toBe('Foobar');
+      expect($('#my_field_1', page).val()).toBe('Foobar');
     });
     it("fills in a given text field by label text when input is nested inside of label", function() {
-      var html = $('<div><label for="some_non_existent_field">My First Field<input /></label></div>');
+      var html = '<div><label for="some_non_existent_field">My First Field<input /></label></div>';
       page = new Capybara.Page(html);
-      expect($('input', html).val()).toBe('');
+      expect($('input', page).val()).toBe('');
       page.fillIn('My First Field').with('Foobar');
-      expect($('input', html).val()).toBe('Foobar');
+      expect($('input', page).val()).toBe('Foobar');
     });
     it("raises if the label can't be found", function() {
-      var html = $('<div></div>');
+      var html = '<div></div>';
       page = new Capybara.Page(html);
       expect(function() { page.fillIn('My First Field').with('Foobar') }).toThrow();
     });
     it("raises if the input can't be found", function() {
-      var html = $('<div><label for="some_non_existent_field">My First Field</label></div>');
+      var html = '<div><label for="some_non_existent_field">My First Field</label></div>';
       page = new Capybara.Page(html);
       expect(function() { page.fillIn('My First Field').with('Foobar') }).toThrow();
     });
